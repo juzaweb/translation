5a341f55ce797dbc0177935ac2f729e8fd3a1c4f
Juzaweb <dangtheanh16@gmail.com>
2024-04-27 10:52:03 +0700

    :+1: Move translation to plugin

--- src/Commands/ImportTranslationCommand.php
+++ src/Commands/ImportTranslationCommand.php
@@ -5,7 +5,7 @@ namespace Juzaweb\Translation\Commands;
 use Juzaweb\CMS\Contracts\TranslationManager;
 use Juzaweb\CMS\Facades\Plugin;
 use Juzaweb\CMS\Facades\ThemeLoader;
-use Juzaweb\DevTool\Commands\Plugin\Translation\ImportTranslationCommand as PluginImportTranslationCommand;
+use Juzaweb\Translation\Commands\Plugins\ImportTranslationCommand as PluginImportTranslationCommand;
 
 class ImportTranslationCommand extends TranslationCommand
 {
